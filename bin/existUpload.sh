20d5978419ac921becdbee8220c8c385c25de1e8
gustavo <gusfer@gmail.com>
2025-02-20 10:00:44 +0100

    Updated existUpload

--- bin/existUpload.sh
+++ bin/existUpload.sh
@@ -3,25 +3,18 @@ file=$1
 path=$2
 reindex=$3
 fbase="$(basename -- $file)"
-echo "starting eXist upload"
-echo "file:"
-echo $file
-echo "path:"
-echo $path
-echo "eXist user:"
-echo ${existuser}
+echo "Starting eXist upload for file ›${file}‹ at path ›${path}‹ with eXist user ›${existuser}‹"
 if [ -z ${existuser} ]; then
-  echo "existuser nicht angegeben"
+  echo "existuser nicht angegeben. Abbrechen"
   exit
 fi
 if [ -z ${existpass} ]; then
-  echo "existpass nicht angegeben"
+  echo "existpass nicht angegeben. Abbrechen"
   exit
 fi
-echo "eXist upload"
 curl -s -S -X PUT -H "Content-type: application/xml" --upload-file $file "http://servhc12.ub.uni-heidelberg.de:8080/exist/rest/db/resources/projects/${path}/${fbase}" -u "${existuser}:${existpass}"
-if [ ! -z ${reindex} ]; then
+if [ -n "${reindex}" ]; then
   echo "trigger reindex"
   curl -s -S "https://digi.ub.uni-heidelberg.de/diglit/_reindex/${reindex}"
 fi
-echo "eXist upload finished"
+echo "eXist upload for ›${path}‹ finished."
