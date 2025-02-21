f9c1bef3e73865c7cc424c5e8eb10d75d466f7e4
gusriva <gusfer@gmail.com>
2025-02-21 11:52:25 +0100

    Added resolution of xinclude to parser

--- src/heipy/parsers.py
+++ src/heipy/parsers.py
@@ -76,6 +76,16 @@ class HeiEditionsParser(et.XMLParser):
         kwargs.setdefault('encoding', 'utf-8')
         super().__init__(*args, **kwargs)
         self.resolvers.add(HeiEditionsResolver())
+           
+    def parse(self, source, base_url=None):
+        tree = super().parse(source, base_url)
+        tree.xinclude()
+        return tree
+    
+    # def close(self):
+    #     tree = super().close()
+    #     tree.xinclude()
+    #     return tree
         
 
 def set_params_for_saxon(parameter_list, proc: PySaxonProcessor, executable):
