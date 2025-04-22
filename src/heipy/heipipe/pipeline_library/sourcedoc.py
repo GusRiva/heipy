09b7967eb870c2cc8541d4ce5338d447abc5bd29
gustavo <gusfer@gmail.com>
2025-04-22 13:18:15 +0200

    Updating tutorial.ipynb

--- src/heipy/heipipe/pipeline_library/sourcedoc.py
+++ src/heipy/heipipe/pipeline_library/sourcedoc.py
@@ -13,27 +13,23 @@ class SourceDocPipe(Pipeline):
         # SourceDoc Pipeline Standard
         pipe_steps = [
             # Index: 0
-            # validation.get_step(),
             initials.get_step(),
             transcription_note.get_step(),
             connect_lb_and_segment.get_step(),
             move_physical_beginnings.get_step(),
-            # Index: 5
             whitespaces.get_step(),
+            # Index: 5
             mark_note_as_editorial_step,
             number_line_segment_beginnings.get_step(),
             container2milestone_step,
             split_word_at_physical_beginnings.get_step(),
-            # Index: 10
             split_everything_at_physical_beginnings.get_step(),
+            # Index: 10
             remove_gap_page.get_step(),
             wrap_resolved_content.get_step(),
             prepare_figure_4_source_doc.get_step(),
             combine_facsimile_and_text_to_sourcedoc.get_step(),
-            # validation.get_step()
             ]
         
-        description = "Source Doc Pipeline - Standard"
+        description = "Source Doc Pipeline"
         super().__init__(steps=pipe_steps, name="sourcedoc_pipe", desc=description, serial=False)
-
-
