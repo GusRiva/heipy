73855355caa72546a14681d588f7e84f438ced2b
gusriva <gusfer@gmail.com>
2025-07-23 17:51:56 +0200

    Improved positioning of pb and cb in synopsis

--- src/heipy/heipipe/step_library/move_layout_milestones.py
+++ src/heipy/heipipe/step_library/move_layout_milestones.py
@@ -7,7 +7,7 @@ from lxml import etree as et
 
 
 def move_milestones(root, parameters):
-    new_container_xpath = '(./following::*[name() = "l" or name() = "titlePart"])[1]'
+    new_container_xpath = '(./following::*[name() = "l" or name() = "titlePart" or name() = "p"])[1]'
     movenda = ['cb', 'pb']
     move_context = [prefix_format('tei', x) for x in ['body', 'front', 'back', 'text', 'lg', 'div', 'ab']]
     for mov in movenda:
@@ -15,11 +15,25 @@ def move_milestones(root, parameters):
             parent_tag = mov_el.getparent().tag
             if parent_tag not in move_context:
                 continue
-            next_l = mov_el.xpath(new_container_xpath, namespaces= ns)
-            if len(next_l) < 1:
+            next_container = mov_el.xpath(new_container_xpath, namespaces= ns)
+            if len(next_container) < 1:
                 continue
-            next_l[0].insert(0, mov_el)
-    return root
+            best_fit = find_better_descendant(next_container[0]) # Sometimes it is better in a descendant of the first following
+            best_fit.insert(0, mov_el)
+    return root  
+
+def find_better_descendant(container):
+    if container.text.strip() != '':
+        return container
+    children = container.getchildren()
+    if len(children) < 1:
+        return container
+    first_child = children[0]
+    if first_child.tag in [prefix_format('tei', x) for x in ['cb', 'lb', 'milestone', 'w']]:
+        # Elements where we don't go further
+        return container
+    return find_better_descendant(first_child)
+
 
 def get_step():
     return PythonStep(
