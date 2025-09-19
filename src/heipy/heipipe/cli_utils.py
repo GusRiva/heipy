f4334d4c3a00b82ef951985a522972a907fa1962
gusriva <gusfer@gmail.com>
2025-09-19 14:23:25 +0200

    Improved cli_utils

--- src/heipy/heipipe/cli_utils.py
+++ src/heipy/heipipe/cli_utils.py
@@ -1,7 +1,21 @@
 import argparse
 
+
+class HeipyCliArgumentParser(argparse.ArgumentParser):
+    def parse_args(self, args=None, namespace=None):        
+        parsed_args = super().parse_args(args, namespace)
+        
+        # Possible flags
+        flags = ["semantic", "sourcedoc", "synoptic"]
+        if not any(getattr(parsed_args, flag) for flag in flags):
+            for flag in flags:
+                setattr(parsed_args, flag, True)
+            
+        return parsed_args
+
+
 def get_cli_parser():
-    parser = argparse.ArgumentParser(
+    parser = HeipyCliArgumentParser(
         description="Shared argument parser for my scripts"
     )
 
@@ -12,3 +26,6 @@ def get_cli_parser():
     parser.add_argument("--sourcedoc", '-sou', action="store_true", help="SourceDoc pipeline", required=False)
 
     return parser
+
+
+
